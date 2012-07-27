a6a1ba35375ad5625d10ba4d8129a71c8f4bd6ee
Meghan Castagno <mdcastagno@gmail.com>
2012-07-27 11:46:45 -0400

    Color Etc

--- Projects/pygame-examples/Predictive_Saccade_Task.py
+++ Projects/pygame-examples/Predictive_Saccade_Task.py
@@ -32,7 +32,7 @@ screen = pygame.display.set_mode(size, pygame.FULLSCREEN)  # make window ## set 
 s_size = s_width, s_height = 35, 35
 s = pygame.Surface(s_size)  # create surface 100px by 50px ##new surface; this is the actual 'physical' box
 ##S is defined in pygame; it is an object and class is pygame.Surface
-s.fill((255, 160, 0))  # color the surface blue #RGB values to color the box
+s.fill((0, 0, 0))  # color the surface blue #RGB values to color the box
 ##Actually a method. Functions attached to objects =def
 r = s.get_rect()  # get the rectangle bounds for the surface
 ##another method. Returns the rectangular bound. Surface has a color and this is the actual bounding of the object
@@ -53,9 +53,10 @@ random.shuffle (cases)
 
 on_case = [False]
 case_times = [0]
-case_stop_times = [3]
+case_stop_times = [7]
 case_over = [False]
-time_between_changes = 0.033333333333333
+time_between_changes = .2
+gap_time = time_between_changes/2
 
 ##defined four functions:
 def handle_events(): #handles events: while it goes it the animate loop
@@ -66,7 +67,7 @@ def handle_events(): #handles events: while it goes it the animate loop
         if (event.type == pygame.KEYUP) or (event.type == pygame.KEYDOWN) :
             print event
             if (event.key == pygame.K_ESCAPE):
-           			sys.exit()
+                    sys.exit()
 
 
 _start_time = time.time()
@@ -94,7 +95,7 @@ def animate():
     now = time.time()
     for i in range (len(cases)): 
         if now -_start_time > case_times [i] and not on_case [i]: 
-            _last_theta = cases [i]
+            _last_theta = cases [i] +180
             on_case[i] = True 
         if now - _start_time > case_stop_times [i] and not case_over [i]: 
             case_over [i] = True
@@ -103,7 +104,8 @@ def animate():
             time.sleep (2) 
             if i + 1 == len(case_over):
                 sys.exit (0)
-        
+    
+    print now, _last_called, time_between_changes, now- _last_called
     if now - _last_called > time_between_changes:  # 2 is in seconds
         # Remember when I was called last
         _last_called = now  ##appears every 2 seconds (goes through 60x)
@@ -116,14 +118,18 @@ def animate():
         x+= width/2.0
         y+=height/2.0
         
-        print theta, x, y 
+    
         
         # pygame.Rect needs 'left', 'top', 'width', 'height'
         # The 100, and the 50 are borrowed from the Surface above
         r = pygame.Rect(x, y, s_width, s_height)
 
+    if now - _last_called > time_between_changes - gap_time: 
+        r = pygame.Rect (-100, -100, 0, 0)
+        return
+        
 def redraw(): #once we move box, we redraw screen, erase screen, redraw box in new position
-    screen.fill((0, 0, 0))  # make redraw background black ##draws a black box
+    screen.fill((255, 255, 255))  # make redraw background black ##draws a black box
     screen.blit(s, r)  # render the surface into the rectangle ##blit is to put it on the screen. takes surface and shape, saws draw surface at this position
     pygame.display.flip()  # update the screen ##dual screen that flips back and forth
 
@@ -131,9 +137,9 @@ def redraw(): #once we move box, we redraw screen, erase screen, redraw box in n
 def main(): ##What tells it to move 30 times a sec
     ##i= 0
     while 1:  # infinite loop
-        clock.tick(30)  # limit framerate to 30 FPS ##clock object prv def.
+        ##clock.tick(10)  # limit framerate to 30 FPS ##clock object prv def.
 
-        handle_events() ##associated with clock.tickstops you from faster 30x a sec; it's s throttle to remember when it needs to refresh itself
+        ##handle_events() ##associated with clock.tickstops you from faster 30x a sec; it's s throttle to remember when it needs to refresh itself
         animate()  ##this function calls these three functions we prv def.
         redraw()
        ## saveimage(i)
